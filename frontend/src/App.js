6d3f9baafb08928835c6dcebb12ea769a5464dc6
moaf <mafaqq318@gmail.com>
2023-10-03 12:09:39 +0300

    Added Header,Footer,HomeScreen React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+
 
 const App = () => {
   return (
-    <h1>Time for Shopzen</h1>
+    <>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
 
