842668ca2d984d96e4df05d801532011eee7774a
moaf <mafaqq318@gmail.com>
2023-10-03 13:01:47 +0300

    completed basic frontend functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from "react-router-bootstrap";
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -8,24 +9,29 @@ const Header = () => {
     <header>
         <Navbar bg="dark" data-bs-theme="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
-                    <img src={logo} alt='Shopzen'></img>
-                Shopzen
-                </Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt='Shopzen'></img>
+                    Shopzen
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/cart">
-                            <FaShoppingCart/>
-                            Cart
-                        </Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link>
+                                <FaShoppingCart/>
+                                Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>
+                                <FaUser/>
+                                Sign In
+                            </Nav.Link>
+                        </LinkContainer>
 
-                        <Nav.Link href="/login">
-                            <FaUser/>
-                            Sign In
-                        </Nav.Link>
                     </Nav>
-                    
                 </Navbar.Collapse>
             </Container>
         </Navbar>
