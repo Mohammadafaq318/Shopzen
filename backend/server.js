edab7f5af1fe296a8a00ac6f7a1a94a8c04483dd
afaq <mafaqq318@gmail.com>
2023-10-09 15:20:23 +0300

    Added database connectivity to backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -15,14 +14,8 @@ app.get('/',(req,res)=>{
     res.send("API is Running");
 });
 
-app.get('/api/products',(req,res)=>{
-    res.send(products);
-});
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p)=>p._id === req.params.id);
-    res.send(product);
-});
+app.use("/api/products",productRoutes);
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`);
