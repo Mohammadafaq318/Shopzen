d9174ba4b95d50af43dc30a2bdb62e6a39df3f0a
afaq <mafaqq318@gmail.com>
2023-10-09 15:30:37 +0300

    added customer error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound,errHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ app.get('/',(req,res)=>{
 
 
 app.use("/api/products",productRoutes);
+app.use(notFound);
+app.use(errHandler);
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`);
