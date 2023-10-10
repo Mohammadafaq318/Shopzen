91f3ec094992be1c37554245659f7f5d46868026
afaq <mafaqq318@gmail.com>
2023-10-10 15:08:51 +0300

    Added Products API slice redux toolkit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,24 +1,18 @@
-import {React,useEffect,useState} from 'react'
 import {Row,Col} from "react-bootstrap";
 import Product from '../components/Product';
-import axios from "axios";
-
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
 
-  const [products,setProducts] = useState([]);
-
-  useEffect(()=>{
-    const fetchProducts = async () =>{
-      const {data} = await axios.get("/api/products");
-      setProducts(data);
-    }
-
-    fetchProducts();
-  },[]);
+  const {data: products, isLoading, isError} = useGetProductsQuery();
 
   return (
     <>
+      {isLoading ? (
+        <h2>....Loading</h2>
+      ): isError ? (<div>
+        {isError?.data?.message || isError.error}
+      </div>):(<>
         <h1>Latest Products</h1>
         <Row>
             {products.map((product)=>(
@@ -27,6 +21,8 @@ const HomeScreen = () => {
                 </Col>
             ))}
         </Row>
+      </>)}
+        
     </>
   )
 }
